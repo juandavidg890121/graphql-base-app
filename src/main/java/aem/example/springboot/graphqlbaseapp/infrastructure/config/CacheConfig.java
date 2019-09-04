9828e1e4cedee2a96df88d093a7cfe4bccb1f9d7
Allens Torres Ruíz <allenstr90@gmail.com>
2019-09-04 13:42:07 -0400

    add constants and cache config

---
+++ src/main/java/aem/example/springboot/graphqlbaseapp/infrastructure/config/CacheConfig.java
@@ -0,0 +1,56 @@
+package aem.example.springboot.graphqlbaseapp.infrastructure.config;
+
+import aem.example.springboot.graphqlbaseapp.infrastructure.dba.model.Authority;
+import aem.example.springboot.graphqlbaseapp.infrastructure.dba.model.User;
+import aem.example.springboot.graphqlbaseapp.infrastructure.dba.repository.UserRepository;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.jsr107.Eh107Configuration;
+import org.hibernate.cache.jcache.ConfigSettings;
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    private final javax.cache.configuration.Configuration<Object, Object> jcacheConfiguration;
+
+    public CacheConfig() {
+        jcacheConfiguration = Eh107Configuration.fromEhcacheCacheConfiguration(
+                CacheConfigurationBuilder.newCacheConfigurationBuilder(Object.class, Object.class,
+                        ResourcePoolsBuilder.heap(1000))
+                        .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofSeconds(3600)))
+                        .build());
+    }
+
+    @Bean
+    public HibernatePropertiesCustomizer hibernatePropertiesCustomizer(javax.cache.CacheManager cacheManager) {
+        return hibernateProperties -> hibernateProperties.put(ConfigSettings.CACHE_MANAGER, cacheManager);
+    }
+
+    @Bean
+    public JCacheManagerCustomizer cacheManagerCustomizer() {
+        return cm -> {
+            createCache(cm, UserRepository.USERS_BY_LOGIN_CACHE);
+            createCache(cm, UserRepository.USERS_BY_EMAIL_CACHE);
+            createCache(cm, User.class.getName());
+            createCache(cm, Authority.class.getName());
+            createCache(cm, User.class.getName() + ".authorities");
+        };
+    }
+
+    private void createCache(javax.cache.CacheManager cm, String cacheName) {
+        javax.cache.Cache<Object, Object> cache = cm.getCache(cacheName);
+        if (cache != null) {
+            cm.destroyCache(cacheName);
+        }
+        cm.createCache(cacheName, jcacheConfiguration);
+    }
+}
---
+++ src/main/java/aem/example/springboot/graphqlbaseapp/infrastructure/config/Constants.java
@@ -0,0 +1,5 @@
+package aem.example.springboot.graphqlbaseapp.infrastructure.config;
+
+public final class Constants {
+    public static final String USERNAME_REGEXP = "^[_.a-zA-Z0-9]*$";
+}
