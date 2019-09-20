2506390c8ebebb951724c2d518d34dd91568ed22
Allens Torres Ruíz <allenstr90@gmail.com>
2019-09-20 15:20:20 -0400

    [TC-1] Liquibase refactorization - add liquibase config by environment - add prod environment - add field activation key

--- src/main/java/aem/example/springboot/graphqlbaseapp/GraphqlBaseAppApplication.java
+++ src/main/java/aem/example/springboot/graphqlbaseapp/GraphqlBaseAppApplication.java
@@ -2,10 +2,13 @@ package aem.example.springboot.graphqlbaseapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableConfigurationProperties({LiquibaseProperties.class})
 public class GraphqlBaseAppApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/aem/example/springboot/graphqlbaseapp/infrastructure/config/LiquibaseConfiguration.java
@@ -0,0 +1,46 @@
+package aem.example.springboot.graphqlbaseapp.infrastructure.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+import java.util.concurrent.Executor;
+
+@Configuration
+public class LiquibaseConfiguration {
+
+    private final Logger log = LoggerFactory.getLogger(LiquibaseConfiguration.class);
+
+    private final Environment env;
+
+    private final CacheManager cacheManager;
+
+    public LiquibaseConfiguration(Environment env, CacheManager cacheManager) {
+        this.env = env;
+        this.cacheManager = cacheManager;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(@Qualifier("taskExecutor") Executor executor,
+                                     DataSource dataSource, LiquibaseProperties liquibaseProperties) {
+
+        // Use liquibase.integration.spring.SpringLiquibase if you don't want Liquibase to start asynchronously
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:db/liquibase-changelog.xml");
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
+        liquibase.setShouldRun(liquibaseProperties.isEnabled());
+        log.debug("Configuring Liquibase");
+        return liquibase;
+    }
+}
